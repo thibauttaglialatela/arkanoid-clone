876d2f1cb7b23e4a89b2575526ec892166d6b58c
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-04-13 12:15:58 +0200

    ajout raquette + début mouvement balle

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 15;
+const gridWidth = 570;
+const gridHeight = 300;
+const ballDiameter = 10;
+let xDirection = 2;
+let yDirection = 2;
+let timerId;
+
+
+const userStart = [230, 10];
+const ballStart = [285, 30];
+let currentUserPos = userStart;
+let currentBallPos = ballStart;
 
 //create Block
 class Block {
@@ -48,3 +60,86 @@ function addBlocks() {
 }
 
 addBlocks();
+
+//add user
+const user = document.createElement('div'); //on crée la div
+user.classList.add('user'); //on rajoute la class user à cette div
+drawUser();
+grid.appendChild(user); //ajouter le nouvel élément au parent grid
+
+function drawUser() {
+	user.style.left = currentUserPos[0] + 'px';
+	user.style.bottom = currentUserPos[1] + 'px';
+}
+
+function drawBall() {
+	ball.style.left = currentBallPos[0] + 'px';
+	ball.style.bottom = currentBallPos[1] + 'px';
+}
+
+//move user
+function moveUser(e) {
+	switch(e.key) {
+		case'ArrowLeft':
+			if (currentUserPos[0] > 0 ) {
+				currentUserPos[0] -= 5;
+				drawUser();
+			}			
+			break;
+		case 'ArrowRight':
+			if (currentUserPos[0] < gridWidth - blockWidth) {
+				currentUserPos[0] += 5;
+				drawUser();
+			}			
+			break;
+		default:
+			alert('please use left or right keys to move');
+	}
+
+}
+
+document.addEventListener('keydown', moveUser);
+
+// add ball
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+grid.appendChild(ball);
+
+//move the ball
+const moveBall = () => {
+	currentBallPos[0] += xDirection;
+	currentBallPos[1] += yDirection;
+	drawBall();
+	checkforCollisions();
+}
+
+timerId = setInterval(moveBall, 30);
+
+// check for collisions
+function checkforCollisions() {
+	//check for wall collisions
+	if (currentBallPos[0] >= (gridWidth - ballDiameter) || currentBallPos[1] >= (gridHeight - ballDiameter)) {
+		changeDirection()
+	}
+
+}
+
+function changeDirection() {
+	if (xDirection === 2 && yDirection === 2) {
+		yDirection = -2;
+		return;
+	}
+	if (xDirection === 2 && yDirection === -2) {
+		xDirection = -2;
+		return;
+	}
+	if(xDirection === -2 && yDirection === -2) {
+		yDirection = 2;
+		return;
+	}
+	if (xDirection === -2 && yDirection === 2) {
+		xDirection = 2;
+		return;
+	}
+}
